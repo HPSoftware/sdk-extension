15b3066f1c44c21bd64226c8b3cb6d37fb2b66a9
Andras Toth <atoth@hpe.com>
2017-05-24 10:12:41 +0300

    Removed predefined constants

--- sdk-usage-examples/src/main/java/com/hpe/adm/nga/sdk/OctaneConnectionConstants.java
+++ sdk-usage-examples/src/main/java/com/hpe/adm/nga/sdk/OctaneConnectionConstants.java
@@ -6,12 +6,12 @@ package com.hpe.adm.nga.sdk;
 public interface OctaneConnectionConstants {
 
     //Server
-    String urlDomain = "http://myd-vm10632.hpeswlab.net:8081";
-    Long sharedspaceId = 1001L;
-    Long workspaceId = 1002L;
+    String urlDomain = "";
+    Long sharedspaceId = -1L;
+    Long workspaceId = -1L;
 
     //Auth
-    String username = "sa@nga";
-    String password = "Welcome1";
+    String username = "";
+    String password = "";
 
 }
